7d4697d1fd16989aacaf839c17ca35b4121829ac
PKBM Bungung Salapang <pkbmbungungsalapang970@gmail.com>
2025-09-16 15:00:44 +0800

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2152,7 +2152,7 @@ const App: React.FC = () => {
         </button>
           <div className="mt-4">
             <a
-              href="https://pkbm-absensi-sukirman.netlify.app"
+              href="https://app-siswa-pkbm.netlify.app/"
               className="block w-full text-center bg-gray-600 hover:bg-gray-700 text-white p-3 rounded-lg transition duration-200"
             >
               Kunjungi Halaman Eksternal
